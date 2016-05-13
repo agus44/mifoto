f6145d7c0d6f15f3ed0dfdcccbd2e769e52164ac
Pablo Barria <pbarria.reyes@gmail.com>
2016-05-13 01:23:09 -0300

    Mensaje de Error

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,7 +19,12 @@
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
           </div>
           @if(Session::has('error'))
-                    <div class="alert alert-danger error-login">
+                    <div class="alert alert-danger alert-dismissible">
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                      <h4>
+                        <i class="icon fa fa-ban"></i>
+                        Datos Incorrectos
+                      </h4>
                         {{ Session::get('error') }}
                     </div>
                         @endif
